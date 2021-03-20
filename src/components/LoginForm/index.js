e1ad9c51757c45b711d338e9b4d8fbdbddf54df0
IGOR <igornesan@gmail.com>
2021-03-20 23:19:03 +0200

    feature: add login form, validation and login container

---
+++ src/components/LoginForm/index.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+import picLogin from 'assets/images/login_pic.jpg';
+import styles from './styles.module.scss';
+
+const LoginForm = () => {
+  const [username, setUserName] = useState('');
+  const [error, setError] = useState(false);
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (username.length < 4 || username.length > 16) {
+      setError(true);
+    } else {
+      setUserName('');
+      setError(false);
+    }
+  };
+  return (
+    <div className="container">
+      <div className={styles.formContainer}>
+        <form onSubmit={onSubmit}>
+          {' '}
+          <img src={picLogin} alt="Login avatar" className={styles.picLogin} />
+          <h4 className="text-center">
+            <span className="text-info">JS Band</span> Store
+          </h4>
+          <div className="form-group">
+            <label htmlFor="username" className="w-100">
+              Name
+              <input
+                type="text"
+                name="username"
+                id="username"
+                value={username}
+                className="form-control w-100"
+                onChange={(e) => setUserName(e.target.value)}
+              />
+              {error && (
+                <small className="text-danger">Username is not valid</small>
+              )}
+            </label>
+          </div>
+          <input
+            type="submit"
+            value="Login"
+            className="btn btn-block btn-info"
+          />
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default LoginForm;
--- src/navigation/Public.js
+++ src/navigation/Public.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Redirect, Route, Switch, useLocation } from 'react-router-dom';
 import LoginContainer from 'containers/LoginContainer';
 import NotFound from 'containers/NotFound';
+
 import goRoute from './goRoute';
 
 const Public = () => {
+  const pathName = useLocation().pathname;
+
   return (
     <>
       <Switch>
         <Route path={goRoute.login} component={LoginContainer} />
+        {pathName === '/' && <Redirect to={goRoute.login} />}
         <Route path={goRoute.notFound} component={NotFound} />
       </Switch>
     </>
