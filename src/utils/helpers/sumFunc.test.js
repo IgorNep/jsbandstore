ae4a6df76fbedc581726083f49ced44d14dd1cee
IGOR <igornesan@gmail.com>
2021-03-23 20:02:26 +0200

    feat: check test

--- src/utils/helpers/sumFunc.test.js
+++ src/utils/helpers/sumFunc.test.js
@@ -4,4 +4,7 @@ describe('Sum function', () => {
   test('should not return 6', () => {
     expect(sum(2, 3)).not.toBe(6);
   });
+  test('should not return 6', () => {
+    expect(sum(3, 3)).toBe(6);
+  });
 });
